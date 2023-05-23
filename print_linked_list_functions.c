20ec35f9e2f24191d5a548aea95e54ce110c6fb3
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-23 09:59:30 +0300

    stack, queue functions

--- print_linked_list_functions.c
+++ print_linked_list_functions.c
@@ -3,18 +3,21 @@
 /**
  * print_ints - prints all the elements in a dll as numbers.
  * @h: pointer to dll.
- * Return: size of dll.
+`* @l: line number.
+ * opcode: pall.
+ * Return: numbers.
  */
-size_t print_ints(const stack_t *h)
+void print_ints(const stack_t **h, int l)
 {
-	size_t count;
+	(void)l;
 
-	count = 0;
-	while (h != NULL)
+	if (!h)
+		return;
+
+	while (*h != NULL)
 	{
-		printf("%d\n", h->n);
-		++count;
-		h = h->next;
+		printf("%d\n", (*h)->n);
+		*h = (*h)->next;
 	}
 	return (count);
 }
@@ -22,21 +25,77 @@ size_t print_ints(const stack_t *h)
 /**
  * print_string - prints all the elements in a dll as chars.
  * @h: pointer to dll.
- * Return: size of dll.
+ * @l: line number.
+ * opcode: pstr.
+ * Return: chars.
  */
-size_t print_string(const stack_t *h)
+void print_string(const stack_t **h, int l)
 {
-	size_t count;
+	(void)l;
 
-	count = 0;
-	if (!h)
+	if (!h || !*h)
 		puts("");
 
-	while (h != NULL && h->n > 0 && h->n < 127)
+	while (*h != NULL && (*h)->n > 0 && (*h)->n < 127)
 	{
-		printf("%c\n", h->n);
-		++count;
-		h = h->next;
+		printf("%c\n", (*h)->n);
+		*h = (*h)->next;
 	}
 	return (count);
+}
+
+/**
+ * pchar_stack - peeks the top of the stack.
+ * @h: pointer to dll.
+ * @l: line number.
+ * opcode: pchar.
+ * Return: void.
+ */
+void pchar_stack(const stack_t **h, int l)
+{
+	int value;
+
+	if (!h || !*h)
+	{
+		printf("L%d: can't pchar, stack empty\n", l);
+		free_stack(*h);
+		free(command);
+		exit(EXIT_STATUS);
+	}
+
+	value = (*h)->n;
+	if (value < 0 || value > 127)
+		printf("L%d: can't pchar, value out of range\n", l);
+	else
+		printf("%c\n", value);
+}
+
+/**
+ * pchar_queue - peeks the back of the queue.
+ * @h: pointer to dll.
+ * @l: line number.
+ * opcode: pchar.
+ * Return: void.
+ */
+void pchar_stack(const stack_t **h, int l)
+{
+	int value;
+
+	if (!h || !*h)
+	{
+		printf("L%d: can't pchar, queue empty\n", l);
+		free_stack(*h);
+		free(command);
+		exit(EXIT_STATUS);
+	}
+
+	/*get to the last element*/
+	while ((*h)->next != NULL)
+		*h = (*h)->next;
+
+	value = (*h)->n;
+	if (value < 0 || value > 127)
+		printf("L%d: can't pchar, value out of range\n", l);
+	else
+		printf("%c\n", value);
 }
