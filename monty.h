8f53ce7f5f1df48698e6795dc66280b333f4fe43
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-24 06:47:48 +0300

    header fixes

--- monty.h
+++ monty.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 extern char *flag;
+#define LINE_LENGTH 32
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -39,15 +41,31 @@ typedef struct instruction_s
 stack_t *add_node(stack_t **head, const int n);
 void free_stack(stack_t *head);
 stack_t *pop_s(stack_t **head);
-stack_t *dequeue(stack_t *head);
-void pall(stack_t **h, int l);
-void pstr(stack_t **h, int l);
-void pchar(stack_t **h, int l);
-void pint(stack_t **h, int l);
-void pop(stack_t **h, int l);
-void push(stack_t **h, int n);
-void swap(stack_t **h, int l);
-void rotl(stack_t **h, int l);
-void rotr(stack_t **h, int l);
+stack_t *dequeue(stack_t **head);
+void pall(stack_t **h, unsigned int l);
+void pstr(stack_t **h, unsigned int l);
+void pchar(stack_t **h, unsigned int l);
+void pint(stack_t **h, unsigned int l);
+void pop(stack_t **h, unsigned int l);
+void push(stack_t **h, char *line, unsigned int l);
+void swap(stack_t **h, unsigned int l);
+void rotl(stack_t **h, unsigned int l);
+void rotr(stack_t **h, unsigned int l);
+int get_argument(stack_t **h, char *opcode, unsigned int l);
+void _add(stack_t **h, unsigned int l);
+void _sub(stack_t **h, unsigned int l);
+void _div(stack_t **h, unsigned int l);
+void _mul(stack_t **h, unsigned int l);
+void _mod(stack_t **h, unsigned int l);
+void stack(stack_t **h, unsigned int l);
+void queue(stack_t **h, unsigned int l);
+void nop(stack_t **h, unsigned int l);
+char *skip_spaces(char *s);
+char *reach_number(char *s);
+int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, int n);
+int _strlen(char *s);
+void execute(stack_t **h, char *line, unsigned int line_number);
+int main(int ac, char **av);
 
 #endif
