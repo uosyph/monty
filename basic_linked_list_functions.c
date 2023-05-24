5804e0adaea1de920f3d9425a793156998522b31
Yousef Saeed <yousafesaeed@gmail.com>
2023-05-25 02:50:08 +0300

    add node func

--- basic_linked_list_functions.c
+++ basic_linked_list_functions.c
@@ -1,35 +1,42 @@
 #include "monty.h"
 
 /**
- * add_node - push or enqueue an element at the beginning of the stack
- * @head: pointer to dll
- * @n: value to add
- * Return: pointer to new node or NULL
+ * add_node - pushs or enqueues an element at the beginning of the stack.
+ * @head: pointer to dll.
+ * @n: value to add.
+ * Return: pointer to new node, or NULL.
  */
 stack_t *add_node(stack_t **head, const int n)
 {
 	stack_t *new;
 
 	if (!head)
+	{
 		return (NULL);
+	}
 
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
+	{
 		return (NULL);
+	}
 
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
 
 	if (*head)
+	{
 		(*head)->prev = new;
+	}
 	*head = new;
 	return (new);
 }
 
 /**
- * free_stack - free a dll of int
- * @head: a pointer to a dll
+ * free_stack - frees a dll of integer value.
+ * @head: pointer to a dll.
+ * Return: void.
  */
 void free_stack(stack_t *head)
 {
@@ -44,48 +51,97 @@ void free_stack(stack_t *head)
 }
 
 /**
- * pop - return the node at the beginning
- * @head: pointer to a dll
- * Return: pointer to the node or NULL
+ * pop_s - gets the node at the beginning.
+ * @head: pointer to dll.
+ * Return: pointer to the node, or NULL.
  */
 stack_t *pop_s(stack_t **head)
 {
 	stack_t *tmp;
 
-	printf("%s\n", __FILE__);
-
 	if (!head || !*head)
+	{
 		return (NULL);
+	}
 
 	tmp = *head;
 	*head = (*head)->next;
-
 	if (*head)
+	{
 		(*head)->prev = NULL;
+	}
 
-	return (tmp); /*remember to free it*/
+	return (tmp);
 }
 
 /**
- * dequeue - return the node at the end
- * @head: pointer to a dll
- * Return: pointer to node or NULL
+ * dequeue - gets the node at the end.
+ * @head: pointer to dll.
+ * Return: pointer to the node, or NULL.
  */
 stack_t *dequeue(stack_t **head)
 {
 	stack_t *h;
 
 	if (!head || !*head)
+	{
 		return (NULL);
+	}
 
 	h = *head;
 	while (h->next != NULL)
+	{
 		h = h->next;
+	}
 
 	if (h->prev)
+	{
 		(h->prev)->next = NULL;
+	}
 	else
+	{
 		*head = NULL;
+	}
 
 	return (h);
+}
+
+/**
+ * add_node_end - adds node at the end of a dll.
+ * @head: pointer to the linked list.
+ * @n: a value to insert.
+ * Return: node address, or NULL.
+ */
+stack_t *add_node_end(stack_t **head, int n)
+{
+	stack_t *new, *tmp;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head)
+	{
+		for (tmp = *head; tmp->next;)
+			tmp = tmp->next;
+		new->prev = tmp;
+		tmp->next = new;
+	}
+	else
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+
+	return (new);
 }
